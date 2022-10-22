391852496f71cfff93821f0955276bec96ece0cd
Anthony Donayre <e.a.donayre95@gmail.com>
2022-10-21 20:44:24 -0500

    darkmode details

--- js/app.js
+++ js/app.js
@@ -4,7 +4,27 @@ const btnSwitch = document.querySelector('#switch');
 btnSwitch.addEventListener('click', () =>{
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
-})
+
+    //LocalStorage para guardar el light o darkmode
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+    } else{
+        localStorage.setItem('dark-mode', 'false');
+    }
+});
+
+//Obtenemos el modo actual
+if(localStorage.getItem('dark-mode') == 'true'){
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+} else{
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+}
+
+//JSON
+const producto1 = { id: 2, producto: "Arroz" };
+localStorage.setItem("producto1", producto1);
 
 // Simulador de cuotas
 
